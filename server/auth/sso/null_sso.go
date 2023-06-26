30bb3211cfbb60fef057c50665ba020b5f5b9d43
Praveen Jindal <pjindal@slb.com>
2023-06-26 12:22:40 +0530

    updated null_sso

--- server/auth/sso/null_sso.go
+++ server/auth/sso/null_sso.go
@@ -15,6 +15,10 @@ func (n nullService) IsRBACEnabled() bool {
 	return false
 }
 
+func (n nullService) GetCallbackURLPath() string {
+	return ""
+}
+
 func (n nullService) Authorize(string) (*types.Claims, error) {
 	return nil, fmt.Errorf("not implemented")
 }
